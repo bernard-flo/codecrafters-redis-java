d806d6ebab670b07e4aab3a713f483b2f607676f
Dongwoon Son <dongwoon.son@dreamus.io>
2023-03-17 11:32:26 +0900

    Handle concurrent clients

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
+import java.util.concurrent.CompletableFuture;
 
 public class Main {
 
@@ -11,62 +12,52 @@ public class Main {
     connection.writeSimpleString("PONG");
   }
 
-  private static void handleClientSocket(Socket socket) throws IOException {
-    Scanner scanner = new Scanner(socket.getInputStream());
-    RedisConnection connection = new RedisConnection(socket.getOutputStream());
+  private static void handleClientSocket(Socket socket) {
+    try {
+      Scanner scanner = new Scanner(socket.getInputStream());
+      RedisConnection connection = new RedisConnection(socket.getOutputStream());
 
-    while (scanner.hasNext()) {
-      String line = scanner.nextLine();
-      System.out.println("line: " + line);
-      if (line.startsWith("ping")) {
-        handlePing(connection, line);
-      } else if (line.startsWith("DOCS")) {
-        connection.writeSimpleString("");
+      while (scanner.hasNext()) {
+        String line = scanner.nextLine();
+        System.out.println("line: " + line);
+        if (line.startsWith("ping")) {
+          handlePing(connection, line);
+        } else if (line.startsWith("DOCS")) {
+          connection.writeSimpleString("");
+        }
+      }
+
+      socket.close();
+      System.out.println("Done");
+    } catch (IOException e) {
+      System.out.println("IOException: " + e.getMessage());
+    } finally {
+      try {
+        socket.close();
+      } catch (IOException e) {
+        System.out.println("IOException: " + e.getMessage());
       }
     }
-
-    socket.close();
-    System.out.println("Done");
   }
 
   public static void main(String[] args){
-    boolean debug;
-    if (args.length >= 1) {
-      debug = true;
-    } else {
-      debug = false;
-    }
-
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
 
     //  Uncomment this block to pass the first stage
     ServerSocket serverSocket = null;
-    Socket clientSocket = null;
     int port = 6379;
     try {
       serverSocket = new ServerSocket(port);
       serverSocket.setReuseAddress(true);
 
       while (true) {
-        // Wait for connection from client.
-        clientSocket = serverSocket.accept();
-        handleClientSocket(clientSocket);
-        if (debug == false) {
-          break;
-        }
+        Socket clientSocket = serverSocket.accept();
+        CompletableFuture.runAsync(() -> handleClientSocket(clientSocket));
       }
 
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
-    } finally {
-      try {
-        if (clientSocket != null) {
-          clientSocket.close();
-        }
-      } catch (IOException e) {
-        System.out.println("IOException: " + e.getMessage());
-      }
     }
   }
 
