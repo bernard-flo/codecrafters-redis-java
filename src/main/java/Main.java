a4f030dd6ca70f54ee9b00666696d3c657459cd2
Dongwoon Son <dongwoon.son@dreamus.io>
2023-03-17 10:09:42 +0900

    Simple pong

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
+
   public static void main(String[] args){
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
@@ -16,6 +17,14 @@ public class Main {
       serverSocket.setReuseAddress(true);
       // Wait for connection from client.
       clientSocket = serverSocket.accept();
+
+      byte[] buffer = new byte[10];
+
+      int read = clientSocket.getInputStream().read(buffer);
+      System.out.println(read + " " + new String(buffer));
+      clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+
+
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
     } finally {
